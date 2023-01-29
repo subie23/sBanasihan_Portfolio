56c2bb886b795b7680a477fc82ab95a77eb16931
Suzanne Banasihan <sjb04@ymail.com>
2023-01-29 09:18:27 -0800

    starter files with updates

---
+++ client/src/PortfolioContainer/Home/Profile.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function Profile() {
+    return (
+        <div className="profile-container">
+            <div className="profile-parent">
+                <div className="profile-details">
+                    <div className="colz">
+                        <a href="https://www.facebook.com/sue.oterobanasihan">
+                           <i className="fa fa-facebook-square"></i> 
+                        </a>
+                        <a href="https://www.instagram.com/subie2328/">
+                           <i className="fa fa-instagram"></i> 
+                        </a>
+                        <a href="https://twitter.com/s_o_banasihan">
+                           <i className="fa fa-twitter"></i> 
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
