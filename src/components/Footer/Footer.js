3f4193f33a0525830a0393a03cbbf9b385a6d358
Suzanne Banasihan <sjb04@ymail.com>
2023-01-30 01:30:45 -0800

    more reworking of components

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { MDBFooter,  MDBIcon } from 'mdb-react-ui-kit';
+
+const Footer = () => {
+    return (
+        <MDBFooter bgColor='light' className='text-center text-lg-start text-muted'>
+            <div className='text-center p-4' style={{ backgroundColor: 'rgba(0, 0, 0, 0.05)' }}>
+                <span>
+                <a rel="noreferrer" href='http://www.linkedin.com/in/sue-banasihan-004b43241/' target="_blank" className='me-4 text-reset'>
+                <MDBIcon fab icon="linkedin" size='2x'/>
+                </a>
+                <a rel="noreferrer" href='https://github.com/subie23' target="_blank" className='me-4 text-reset'>
+                <MDBIcon fab icon="github" size='2x'/>
+                </a>
+                <a rel="noreferrer" href='https://https://instagram.com/subie2328' target="_blank" className='me-4 text-reset'>
+                <MDBIcon fab icon="instagram" size='2x'/>
+                </a>
+                </span>
+                <span className="px-4">© 2023 Copyright</span>
+            </div>
+        </MDBFooter>
+    );
+};
+
+export default Footer;
+
